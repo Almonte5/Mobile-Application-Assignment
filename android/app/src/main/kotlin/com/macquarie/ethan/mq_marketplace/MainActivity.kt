51fcc0b373de304996f65b23f957e1afa9c0a0d2
Ethan Almonte <ethan.almonte@students.mq.edu.au>
2026-04-29 23:15:14 +1000

    Created the flutter project scaffold, dependencies and folder structure

---
+++ android/app/src/main/kotlin/com/macquarie/ethan/mq_marketplace/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.macquarie.ethan.mq_marketplace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MQMarketplaceApp());
+}
+
+class MQMarketplaceApp extends StatelessWidget {
+  const MQMarketplaceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MQ Marketplace',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF8C1A4B)),
+        useMaterial3: true,
+      ),
+      home: const PlaceholderHome(),
+    );
+  }
+}
+
+class PlaceholderHome extends StatelessWidget {
+  const PlaceholderHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MQ Marketplace')),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Text(
+            'Project scaffolded. Auth and listings coming soon.',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
